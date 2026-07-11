dbc25938533d187fe063323bb0f812698a6a08b4
chiru199231 <srai95273@gmail.com>
2026-07-11 11:13:28 +0530

    Update players.js

--- players.js
+++ players.js
@@ -1,163 +1,30 @@
 const tickets = [
 
-{
-ticket:1,
-code:"GOAT",
-argentina:"Facundo Medina",
-switzerland:"Ulisses Garcia"
-},
+{ticket:1,argentina:"Facundo Medina",switzerland:"Ulisses Garcia"},
+{ticket:2,argentina:"Juan Foyth",switzerland:"Fabian Rieder"},
+{ticket:3,argentina:"Leonardo Balerdi",switzerland:"Filip Ugrinic"},
+{ticket:4,argentina:"Nicolas Paz",switzerland:"Cedric Zesiger"},
+{ticket:5,argentina:"Valentin Barco",switzerland:"Gregory Wuthrich"},
+{ticket:6,argentina:"Thiago Almada",switzerland:"Silvan Widmer"},
+{ticket:7,argentina:"Exequiel Palacios",switzerland:"Michel Aebischer"},
+{ticket:8,argentina:"Guido Rodriguez",switzerland:"Ardon Jashari"},
+{ticket:9,argentina:"German Pezzella",switzerland:"Zeki Amdouni"},
+{ticket:10,argentina:"Nicolas Gonzalez",switzerland:"Kwadwo Duah"},
+{ticket:11,argentina:"Giuliano Simeone",switzerland:"Andi Zeqiri"},
+{ticket:12,argentina:"Angel Correa",switzerland:"Ruben Vargas"},
+{ticket:13,argentina:"Leandro Paredes",switzerland:"Nico Elvedi"},
 
-{
-ticket:2,
-code:"GOAT",
-argentina:"Juan Foyth",
-switzerland:"Fabian Rieder"
-},
+// ⭐ Better players after Ticket 13
 
-{
-ticket:3,
-code:"GOAT",
-argentina:"Leonardo Balerdi",
-switzerland:"Filip Ugrinic"
-},
+{ticket:14,argentina:"Nicolas Otamendi",switzerland:"Ricardo Rodriguez"},
+{ticket:15,argentina:"Nahuel Molina",switzerland:"Dan Ndoye"},
+{ticket:16,argentina:"Lisandro Martinez",switzerland:"Manuel Akanji"},
+{ticket:17,argentina:"Cristian Romero",switzerland:"Djibril Sow"},
+{ticket:18,argentina:"Rodrigo De Paul",switzerland:"Remo Freuler"},
+{ticket:19,argentina:"Alexis Mac Allister",switzerland:"Denis Zakaria"},
+{ticket:20,argentina:"Enzo Fernandez",switzerland:"Granit Xhaka"},
+{ticket:21,argentina:"Julian Alvarez",switzerland:"Breel Embolo"},
+{ticket:22,argentina:"Lautaro Martinez",switzerland:"Noah Okafor"},
+{ticket:23,argentina:"Lionel Messi",switzerland:"Granit Xhaka"}
 
-{
-ticket:4,
-code:"GOAT",
-argentina:"Nicolas Paz",
-switzerland:"Cedric Zesiger"
-},
-
-{
-ticket:5,
-code:"GOAT",
-argentina:"Valentin Barco",
-switzerland:"Gregory Wuthrich"
-},
-
-{
-ticket:6,
-code:"GOAT",
-argentina:"Thiago Almada",
-switzerland:"Silvan Widmer"
-},
-
-{
-ticket:7,
-code:"GOAT",
-argentina:"Exequiel Palacios",
-switzerland:"Michel Aebischer"
-},
-
-{
-ticket:8,
-code:"GOAT",
-argentina:"Guido Rodriguez",
-switzerland:"Ardon Jashari"
-},
-
-{
-ticket:9,
-code:"GOAT",
-argentina:"German Pezzella",
-switzerland:"Zeki Amdouni"
-},
-
-{
-ticket:10,
-code:"GOAT",
-argentina:"Nicolas Gonzalez",
-switzerland:"Kwadwo Duah"
-},
-
-{
-ticket:11,
-code:"GOAT",
-argentina:"Giuliano Simeone",
-switzerland:"Andi Zeqiri"
-},
-
-{
-ticket:12,
-code:"GOAT",
-argentina:"Angel Correa",
-switzerland:"Ruben Vargas"
-},
-
-{
-ticket:13,
-code:"GOAT",
-argentina:"Leandro Paredes",
-switzerland:"Nico Elvedi"
-},
-
-{
-ticket:14,
-code:"GOAT",
-argentina:"Nicolas Otamendi",
-switzerland:"Ricardo Rodriguez"
-},
-
-{
-ticket:15,
-code:"GOAT",
-argentina:"Nahuel Molina",
-switzerland:"Dan Ndoye"
-},
-
-{
-ticket:16,
-code:"GOAT",
-argentina:"Lisandro Martinez",
-switzerland:"Manuel Akanji"
-},
-
-{
-ticket:17,
-code:"GOAT",
-argentina:"Cristian Romero",
-switzerland:"Djibril Sow"
-},
-
-{
-ticket:18,
-code:"GOAT",
-argentina:"Rodrigo De Paul",
-switzerland:"Remo Freuler"
-},
-
-{
-ticket:19,
-code:"GOAT",
-argentina:"Alexis Mac Allister",
-switzerland:"Denis Zakaria"
-},
-
-{
-ticket:20,
-code:"GOAT",
-argentina:"Enzo Fernandez",
-switzerland:"Granit Xhaka"
-},
-
-{
-ticket:21,
-code:"GOAT",
-argentina:"Julian Alvarez",
-switzerland:"Breel Embolo"
-},
-
-{
-ticket:22,
-code:"GOAT",
-argentina:"Lautaro Martinez",
-switzerland:"Noah Okafor"
-},
-
-{
-ticket:23,
-code:"GOAT",
-argentina:"Lionel Messi",
-switzerland:"Granit Xhaka"
-}
 ];
