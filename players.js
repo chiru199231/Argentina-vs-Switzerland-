1ffbed43f2283964f5175a9cce0bb11f3c96df8c
chiru199231 <srai95273@gmail.com>
2026-07-11 11:01:10 +0530

    Add ticket data for Argentina and Switzerland players

---
+++ players.js
@@ -0,0 +1,163 @@
+const tickets = [
+
+{
+ticket:1,
+code:"GOAT",
+argentina:"Facundo Medina",
+switzerland:"Ulisses Garcia"
+},
+
+{
+ticket:2,
+code:"GOAT",
+argentina:"Juan Foyth",
+switzerland:"Fabian Rieder"
+},
+
+{
+ticket:3,
+code:"GOAT",
+argentina:"Leonardo Balerdi",
+switzerland:"Filip Ugrinic"
+},
+
+{
+ticket:4,
+code:"GOAT",
+argentina:"Nicolas Paz",
+switzerland:"Cedric Zesiger"
+},
+
+{
+ticket:5,
+code:"GOAT",
+argentina:"Valentin Barco",
+switzerland:"Gregory Wuthrich"
+},
+
+{
+ticket:6,
+code:"GOAT",
+argentina:"Thiago Almada",
+switzerland:"Silvan Widmer"
+},
+
+{
+ticket:7,
+code:"GOAT",
+argentina:"Exequiel Palacios",
+switzerland:"Michel Aebischer"
+},
+
+{
+ticket:8,
+code:"GOAT",
+argentina:"Guido Rodriguez",
+switzerland:"Ardon Jashari"
+},
+
+{
+ticket:9,
+code:"GOAT",
+argentina:"German Pezzella",
+switzerland:"Zeki Amdouni"
+},
+
+{
+ticket:10,
+code:"GOAT",
+argentina:"Nicolas Gonzalez",
+switzerland:"Kwadwo Duah"
+},
+
+{
+ticket:11,
+code:"GOAT",
+argentina:"Giuliano Simeone",
+switzerland:"Andi Zeqiri"
+},
+
+{
+ticket:12,
+code:"GOAT",
+argentina:"Angel Correa",
+switzerland:"Ruben Vargas"
+},
+
+{
+ticket:13,
+code:"GOAT",
+argentina:"Leandro Paredes",
+switzerland:"Nico Elvedi"
+},
+
+{
+ticket:14,
+code:"GOAT",
+argentina:"Nicolas Otamendi",
+switzerland:"Ricardo Rodriguez"
+},
+
+{
+ticket:15,
+code:"GOAT",
+argentina:"Nahuel Molina",
+switzerland:"Dan Ndoye"
+},
+
+{
+ticket:16,
+code:"GOAT",
+argentina:"Lisandro Martinez",
+switzerland:"Manuel Akanji"
+},
+
+{
+ticket:17,
+code:"GOAT",
+argentina:"Cristian Romero",
+switzerland:"Djibril Sow"
+},
+
+{
+ticket:18,
+code:"GOAT",
+argentina:"Rodrigo De Paul",
+switzerland:"Remo Freuler"
+},
+
+{
+ticket:19,
+code:"GOAT",
+argentina:"Alexis Mac Allister",
+switzerland:"Denis Zakaria"
+},
+
+{
+ticket:20,
+code:"GOAT",
+argentina:"Enzo Fernandez",
+switzerland:"Granit Xhaka"
+},
+
+{
+ticket:21,
+code:"GOAT",
+argentina:"Julian Alvarez",
+switzerland:"Breel Embolo"
+},
+
+{
+ticket:22,
+code:"GOAT",
+argentina:"Lautaro Martinez",
+switzerland:"Noah Okafor"
+},
+
+{
+ticket:23,
+code:"GOAT",
+argentina:"Lionel Messi",
+switzerland:"Granit Xhaka"
+}
+];
